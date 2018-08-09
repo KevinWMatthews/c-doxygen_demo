20fb12183685ff82e3de2fd388e17069c0d3068f
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-08-09 09:06:35 -0700

    Add basic documentation

--- README.md
+++ README.md
@@ -17,13 +17,37 @@ Edit a few settings:
   * `PROJECT_NUMBER`
   * `INPUT`
   * `FILE_PATTERNS`
+  * `EXTRACT_ALL`
   * `HAVE_DOT`
   * `GENERATE_LATEX`
   * `OPTIMIZE_OUTPUT_FOR_C`
   * `BUILTIN_STL_SUPPORT`
+
+Also consider:
+
+  * `JAVADOC_AUTOBRIEF`
+  * `QT_AUTOBRIEF`
 
 This will create the foler `html`.
 To view the documentation, run
 ```
 $ firefox html/index.html &
 ```
+
+If you haven't documented your file/module/class, Doxygen won't parse anything.
+Either do that or set `EXTRACT_ALL` to `YES`.,W
+
+
+### Links
+
+[Basic](http://www.stack.nl/~dimitri/doxygen/manual/docblocks.html)
+[FAQ](https://www.stack.nl/~dimitri/doxygen/manual/faq.html)
+[Special Commands](https://www.stack.nl/~dimitri/doxygen/manual/commands.html)
+
+
+### TODO
+
+Check out
+  * `\code`
+  * `\class`
+  * `\namespace`
--- src/module.h
+++ src/module.h
@@ -1,6 +1,47 @@
 #ifndef MODULE_INCLUDED
 #define MODULE_INCLUDED
 
+// Can use '@' or '\'
+
+/**
+ * @headerfile module.h "module.h"
+ *
+ * Documentation for the file.
+ * Could also use @file?
+ */
+
+/**
+ * @brief   Execute the first function in the module.
+ *
+ * This is a the body of the description.
+ * This function takes no arguments, does nothing, and returns nothing.
+ * It simply provides Doxygen with something to document.
+ *
+ * @return  A descsription of the returned value - in this case, nothing.
+ */
 void module_function_1();
+
+/**
+ * @brief   Execute the second function in the module.
+ *
+ * This function takes an argument but does nothing.
+ * It simply provides Doxygen with something to document.
+ *
+ * @param   the_param   This is the parameter.
+ * @return  Nothing.
+ */
+void module_function_2(int the_param);
+
+/**
+ * @brief   Execute the third function in the moudle.
+ *
+ * This function takes an input and an output argument but does nothing.
+ * It simply provides Doxygen with something to document.
+ * Format: @<param>[direction]    <name>  <Description>
+ *
+ * @param[in]   input   This is the input parameter.
+ * @param[out]  output  This is the output parameter.
+ */
+void module_function_3(int input, int *output);
 
 #endif
